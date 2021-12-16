d8f8ea67a3e5318cec48e13960cea042c71822a7
praveenpro587 <42086848+praveenpro587@users.noreply.github.com>
2021-12-16 16:58:38 +0530

    Update BroadcastJoinDF.scala

--- Spark_RDDs_DFs/BroadcastJoinDF.scala
+++ Spark_RDDs_DFs/BroadcastJoinDF.scala
@@ -4,6 +4,7 @@ import org.apache.log4j.Logger
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
 
 object BroadcastJoinDF extends App{
   
@@ -17,10 +18,20 @@ object BroadcastJoinDF extends App{
   .config(sparkConf)
   .getOrCreate()
   
+  //If we use expliict Schema it will improve performance W.R.T time as it don't need extra time for inferrring schema
+  val Orderschema=StructType(List(
+      StructField("order_id",IntegerType,false),
+      StructField("order_date",TimestampType,true),
+      StructField("Customer_order_id",LongType,true),
+      StructField("Status",StringType,true)
+      ))
+      
+      
+      
   val OrdersDf=spark.read
   .format("csv")
   .option("header", true)
-  .option("inferschema", true)
+  .schema(Orderschema)
   .option("path", "C:/Users/prave/OneDrive/Desktop/Trendy Tech/Spark-Week4/orders.csv")
   .load
   
@@ -33,11 +44,11 @@ object BroadcastJoinDF extends App{
   
   spark.sql("SET spark.sql.autoBroadcastJoinThreshold=-1")
   
-  val joinCon=OrdersDf.col("order_customer_id")===CustomersDF.col("customer_id")
+  val joinCon=OrdersDf.col("Customer_order_id")===CustomersDF.col("customer_id")
   
   val JoinDf=OrdersDf.join(broadcast(CustomersDF),joinCon,"inner")
   
-  JoinDf.show()
+  JoinDf.show(false)
   
   
 }
