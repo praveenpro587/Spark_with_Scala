7672846cf506b3e6e186de99dcc72fea89b7ffab
praveenpro587 <42086848+praveenpro587@users.noreply.github.com>
2021-12-16 13:00:11 +0530

    Create GroupAggEx.scala

---
+++ Spark_RDDs_DFs/GroupAggEx.scala
@@ -0,0 +1,49 @@
+package demo
+
+import org.apache.log4j.Level
+import org.apache.log4j.Logger
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+
+object GroupAggEx extends App{
+  
+   Logger.getLogger("org").setLevel(Level.ERROR)
+  
+   val sparkConf=new SparkConf()
+  sparkConf.set("spark.app.name", "DFAssign")
+  sparkConf.set("spark.master", "local[*]")
+  
+  val spark=SparkSession.builder()
+  .config(sparkConf)
+  .getOrCreate()
+  
+  val OrdersDf=spark.read
+  .format("csv")
+  .option("header", true)
+  .option("path", "C:/Users/prave/OneDrive/Desktop/Trendy Tech/Spark-Week4/order_data.csv")
+  .load
+  
+  //Object Expression
+  OrdersDf.groupBy("Country", "InvoiceNo")
+  .agg(
+      sum("Quantity").as("Total_Quantity"),
+      sum(expr("Quantity * UnitPrice")).as("Invoice_value")
+      ).show(false)
+      
+      
+  //String Expression
+  OrdersDf.groupBy("Country", "InvoiceNo")
+  .agg(expr("sum(Quantity) as Total_Quantity"),
+      expr("sum(Quantity * UnitPrice) as Invoice_value")
+      ).show(false)
+      
+  //Spark SQL     
+ OrdersDf.createOrReplaceTempView("InvoiceData")
+ spark.sql("""select Country,InvoiceNo,sum(Quantity) as Total_Quantity,
+              sum(Quantity * UnitPrice) as Invoice_value from InvoiceData
+              group by Country,InvoiceNo""").show(false)
+  
+  
+}
