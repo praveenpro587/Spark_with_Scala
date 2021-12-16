53807f8a173bc4011ede299046f499cb155e6b3b
praveenpro587 <42086848+praveenpro587@users.noreply.github.com>
2021-12-16 12:55:08 +0530

    Create CleaningEmptyLines.scala

---
+++ Spark_RDDs_DFs/CleaningEmptyLines.scala
@@ -0,0 +1,15 @@
+package demo
+
+import org.apache.log4j.Level
+import org.apache.log4j.Logger
+import org.apache.spark.SparkContext
+object CleaningEmptyLines {
+  
+  Logger.getLogger("org").setLevel(Level.ERROR)
+  
+  val sc=new SparkContext("local[*]","RemEmpLines")
+  val filerdd=sc.textFile("C:/Users/prave/OneDrive/Desktop/Trendy Tech/Spark-Week2/samplefile.txt")
+  val cleanrdd=filerdd.filter(x=>(!x.isEmpty))
+  cleanrdd.collect.foreach(println)
+  
+}
