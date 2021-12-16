c7820824dd2d693267d1aa78d453185fc7be6788
praveenpro587 <42086848+praveenpro587@users.noreply.github.com>
2021-12-16 16:12:04 +0530

    Update BroadcastMoviesDS.scala

--- Spark_RDDs_DFs/BroadcastMoviesDS.scala
+++ Spark_RDDs_DFs/BroadcastMoviesDS.scala
@@ -4,7 +4,7 @@ import org.apache.log4j.Level
 import org.apache.log4j.Logger
 import org.apache.spark.SparkContext
 import scala.io.Source
-object BroadcastMoviesDS {
+object BroadcastMoviesDS extends App{
   
   Logger.getLogger("org").setLevel(Level.ERROR)
   
@@ -16,8 +16,9 @@ object BroadcastMoviesDS {
   val splitrdd=ratingsRdd.map(x=>(x.split("::")(1),x.split("::")(2)))
   
   
-  val moviesRDD=Source.fromFile("C:/Users/prave/OneDrive/Desktop/Trendy Tech/Spark-Week3/movies.dat").getLines().map(x=>(x.split("::")(0),x.split("::")(1)))
-  val braodcast=sc.broadcast(moviesRDD)
+  val moviesRDD=sc.textFile("C:/Users/prave/OneDrive/Desktop/Trendy Tech/Spark-Week3/movies.dat")
+  val bcast=sc.broadcast(moviesRDD.map(x=>(x.split("::")(0),x.split("::")(1))).collect().toMap)
+  //val braodcast=sc.broadcast(moviesRDD)
   // get the o/p=>(100,(4.0,1.0)),(100,(5.0,1.0))
   val newMapped=splitrdd.mapValues(x=>(x.toFloat,1.0))
   //get the o/p=>(100,(9.0,2.0))
@@ -31,13 +32,13 @@ object BroadcastMoviesDS {
   val ratingsProcessed=filterRDD.mapValues(x=>(x._1/x._2)).filter(x=>x._2>4.5)
   
   
+  //ratingsProcessed.collect.foreach(println)
   
-  //Joining RatingRDD and MoviesRDD
-  /*val JoinedRDD=ratingsProcessed.join(braodcast)
+  val FinalRdd=ratingsProcessed.map(x=>(x._1,x._2,bcast.value(x._1)))
   
-  //Getting only movie names and respective rating watched more than 1000
-  val finalRDD=JoinedRDD.map(x=>(x._2._1,x._2._2))
+  FinalRdd.collect.foreach(println)
   
-  finalRDD.collect.foreach(println)*/
+  
+  
   
 }
