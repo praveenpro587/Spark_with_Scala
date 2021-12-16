6b846aa80b6ea94d94416eb5e4d209f99440ae4e
praveenpro587 <42086848+praveenpro587@users.noreply.github.com>
2021-12-16 13:12:09 +0530

    Create UsingAnynFunc.scala

---
+++ Spark_RDDs_DFs/UsingAnynFunc.scala
@@ -0,0 +1,44 @@
+package demo
+
+import org.apache.log4j.Level
+import org.apache.log4j.Logger
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql._
+
+object UsingAnynFunc extends App{
+  
+  Logger.getLogger("org").setLevel(Level.ERROR)
+  
+   val sparkConf=new SparkConf()
+  sparkConf.set("spark.app.name", "DFAssign")
+  sparkConf.set("spark.master", "local[*]")
+  
+  val spark=SparkSession.builder()
+  .config(sparkConf)
+  .getOrCreate()
+  
+  val schema="name String,Age Int,City String"
+  
+  val DF=spark.read
+  .format("csv")
+  .schema(schema)
+  .option("path", "C:/Users/prave/OneDrive/Desktop/Trendy Tech/Spark-Week4/dataset1")
+  .load
+  
+  //Anonymous Function
+  spark.udf.register("parseAgeFunc",(x:Int)=>{
+    if(x>18) "Y" else "N"
+  })
+  
+  DF.withColumn("Adult", expr("parseAgeFunc(age)")).show()
+  
+  spark.catalog.listFunctions().filter(x=>x.name=="parseAgeFunc").show()
+  
+  
+  //If functon register to the catalog so directly we can use it in SQL query
+  DF.createOrReplaceTempView("PeopleTable")
+  spark.sql("select name,age,city,parseAgeFunc(age) as Adult from PeopleTable").show()
+  
+}
