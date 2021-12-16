8114c9c735e1d10d3a300d0deacbb818c77b677b
praveenpro587 <42086848+praveenpro587@users.noreply.github.com>
2021-12-16 12:53:07 +0530

    Create AddsDataProblem.scala

---
+++ Spark_RDDs_DFs/AddsDataProblem.scala
@@ -0,0 +1,22 @@
+package demo
+
+import org.apache.log4j.Level
+import org.apache.log4j.Logger
+import org.apache.spark.SparkContext
+import scala.io.Source
+
+object AddsDataProblem extends App{
+  
+  
+  Logger.getLogger("org").setLevel(Level.ERROR)
+  
+  val sc=new SparkContext("local[*]","AddsData")
+  
+  val filerdd=sc.textFile("C:/Users/prave/OneDrive/Desktop/Trendy Tech/Spark-Week2/bigdatacampaigndata.csv")
+  val maprdd=filerdd.map(x=>(x.split(",")(10).toFloat,x.split(",")(0)))
+  val flatrdd=maprdd.flatMapValues(x=>x.split(" "))
+  val words=flatrdd.map(x=>(x._2,x._1))
+  val finalrdd=words.reduceByKey(_+_)
+  val res=finalrdd.sortBy(x=>x._2,false)
+  res.take(20).foreach(println)
+}
