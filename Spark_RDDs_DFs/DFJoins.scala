ff624d405f359e676867bddfb68c2a162fb761ad
praveenpro587 <42086848+praveenpro587@users.noreply.github.com>
2021-12-16 12:57:27 +0530

    Create DFJoins.scala

---
+++ Spark_RDDs_DFs/DFJoins.scala
@@ -0,0 +1,41 @@
+package demo
+
+import org.apache.log4j.Level
+import org.apache.log4j.Logger
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.functions._
+
+object DFJoins extends App{
+  
+  Logger.getLogger("org").setLevel(Level.ERROR)
+  
+   val sparkConf=new SparkConf()
+  sparkConf.set("spark.app.name", "DFAssign")
+  sparkConf.set("spark.master", "local[*]")
+  
+  val spark=SparkSession.builder()
+  .config(sparkConf)
+  .getOrCreate()
+  
+  val OrdersDf=spark.read
+  .format("csv")
+  .option("header", true)
+  .option("inferschema", true)
+  .option("path", "C:/Users/prave/OneDrive/Desktop/Trendy Tech/Spark-Week4/orders.csv")
+  .load
+  
+   val CustomersDF=spark.read
+  .format("csv")
+  .option("header", true)
+  .option("inferschema", true)
+  .option("path", "C:/Users/prave/OneDrive/Desktop/Trendy Tech/Spark-Week4/customers.csv")
+  .load
+  
+  val joinCon=OrdersDf.col("order_customer_id")===CustomersDF.col("customer_id")
+  val JoinDF=OrdersDf.join(CustomersDF,joinCon,"outer")
+  .sort("order_id")
+  .withColumn("order_id", expr("coalesce(order_id,-1)"))
+  JoinDF.show(false)
+  
+}
