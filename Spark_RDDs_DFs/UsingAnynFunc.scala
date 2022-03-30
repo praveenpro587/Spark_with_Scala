bd5cacefe0ea0e1ab0ae2c2e2f7431fdd59ea0ae
praveenpro587 <42086848+praveenpro587@users.noreply.github.com>
2022-03-30 16:43:04 +0530

    Update UsingAnynFunc.scala

--- Spark_RDDs_DFs/UsingAnynFunc.scala
+++ Spark_RDDs_DFs/UsingAnynFunc.scala
@@ -24,7 +24,7 @@ object UsingAnynFunc extends App{
   val DF=spark.read
   .format("csv")
   .schema(schema)
-  .option("path", "C:/Users/prave/OneDrive/Desktop/Trendy Tech/Spark-Week4/dataset1")
+  .option("path", "C:/Users/prave/OneDrive/Desktop/dataset1")
   .load
   
   //Anonymous Function
